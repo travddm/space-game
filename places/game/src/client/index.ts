572e973018fce935491f170aa5cfff7cd7841adc
travddm <travddm@hotmail.com>
2025-05-22 01:44:04 -0600

    Remove client temp function

--- places/game/src/client/index.ts
+++ places/game/src/client/index.ts
@@ -1,9 +1,6 @@
-import { Players } from "@rbxts/services";
-
 import { log } from "common/shared/log";
 
 import { fixedTimeStep, maxTimeStep } from "shared/constants";
-import { queueAction } from "shared/ecs";
 
 import { clientSystems, startScheduler } from "./ecs";
 import { flushInput, startInputManager } from "./input";
@@ -20,30 +17,4 @@ export function main() {
 	startInputManager();
 
 	log.info("Finished initializing client");
-
-	log.info("Running temp function");
-
-	temp();
-
-	log.info("Finished running temp function");
-}
-
-function temp() {
-	queueAction("addEntity", {
-		entityId: 0,
-		components: {
-			player: tostring(Players.LocalPlayer.UserId),
-			transform: CFrame.identity,
-			movable: {
-				moveSpeed: 30,
-				moveDirection: Vector3.zero,
-				moveVelocity: Vector3.zero,
-				rotateSpeed: math.rad(540),
-				rotateDirection: Vector3.xAxis,
-			},
-			ship: {
-				name: "Big Gamer",
-			},
-		},
-	});
 }
