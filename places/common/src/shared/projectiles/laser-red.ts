9731ab666afe999474fb2c99a0e2195facac32c1
travddm <travddm@hotmail.com>
2025-05-27 22:42:11 -0600

    Add size property to ProjectileType

--- places/common/src/shared/projectiles/laser-red.ts
+++ places/common/src/shared/projectiles/laser-red.ts
@@ -4,4 +4,5 @@ import { ProjectileType } from "./projectile-type";
 export const laserRed: ProjectileType = {
 	assetId: assets["images/projectiles/laser-bullet.png"],
 	color: new Color3(1, 0.2, 0.2),
+	size: new Vector2(0.25, 1),
 };
--- places/common/src/shared/projectiles/projectile-type.ts
+++ places/common/src/shared/projectiles/projectile-type.ts
@@ -1,4 +1,5 @@
 export interface ProjectileType {
 	assetId: string;
 	color: Color3;
+	size: Vector2;
 }
